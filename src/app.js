2acd0d475301716879021d3afe776564f422f8d7
Nay0ne <nayone606@googlemail.com>
2023-05-10 12:29:07 +0100

    posyt and put framework

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
-
+//  to parse request body containing JSON objs-
+app.use(express.json());
+// to parse request body with URL encoded values-
+app.use(express.urlencoded({extended:true}));
 //TODO: Create your GET Request Route Below: 
 app.get('/restaurants', async (req, res) => {
     try {
@@ -12,11 +15,33 @@ app.get('/restaurants', async (req, res) => {
         next(error);
     }
 });
-//  get particular restaurant
+//  get particular restaurant- Epress Restaurantds pt 2
 app.get('/restaurants/:id', async (req, res) => {
-    const id = req.params.id;
-    gimme = await Restaurant.findByPk(id);
-    res.json(gimme);
+   try { 
+        const id = req.params.id;
+        gimme = await Restaurant.findByPk(id);
+        res.json(gimme);
+    } catch(error) {
+        next(error);
+    }
+});
+
+// add new restaurant
+app.post('/restaurants', async (req, res, next) => {
+    try {
+
+    } catch(error) {
+        next(error);
+    }
+});
+
+//  update a restaurant
+app.put('/restaurants', async (req, res) => {
+    try {
+
+    } catch(error) {
+        next(error);
+    }
 });
 
 
